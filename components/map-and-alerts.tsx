378f59972a378ef91ad680158ab317ec2270a23f
M7mod77 <m7modsallal77@gmail.com>
2026-06-01 22:13:00 +0300

    Remove CityMap component from MapAndAlerts; simplify rendering to only include AlertPanel

--- components/map-and-alerts.tsx
+++ components/map-and-alerts.tsx
@@ -1,11 +1,5 @@
-import CityMap from "@/components/city-map"
 import AlertPanel from "@/components/alert-panel"
 
 export default function MapAndAlerts() {
-  return (
-    <div className="space-y-4">
-      <CityMap />
-      <AlertPanel />
-    </div>
-  )
+  return <AlertPanel />
 }
